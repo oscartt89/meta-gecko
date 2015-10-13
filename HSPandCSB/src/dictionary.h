0d418d9a8bde3c028a87b43dabdb45618383ff89
Fernando Moreno Jabato <fmjabato@gmail.com>
2015-10-13 19:40:40 +0200

    Añadida funcionalidad de ordenar array de Words. Nota: quicksort simple. No se ha re-utilizado el quicksort antiguo porque los threads que genera son sólo para SO basados en Linux. Se planteará adaptarlo.

--- HSPandCSB/src/dictionary.h
+++ HSPandCSB/src/dictionary.h
@@ -10,6 +10,7 @@
 static const int  WORD_SIZE = 32;
 static const int BYTES_IN_WORD = 8;
 static const uint64_t MAX_WORDS = 1000000;
+static const int MAX_NUM_PROCS = 2;
 
 //STRUCTS
 typedef struct {
@@ -32,9 +33,9 @@ typedef struct {
 
 
 // FUNCTIONS
-// dictionary.c 
-void takewords(wentry**,char*);
-
-//DictionaryFun.c
+int takewords(wentry**,char*);
 void shift_word(word*);
 int storeWord(wentry**,wentry*,int);
+int partition(wentry**,int,int);
+int quickSort(wentry**,int,int);
+int wordComparator(wentry*,wentry*);
--- HSPandCSB/src/dictionaryFun.c
+++ HSPandCSB/src/dictionaryFun.c
@@ -1,5 +1,99 @@
 #include "dictionary.h"
 
+
+/* This method implements the process of open the file, read the
+ * sequences and take the words.
+ * @param words is an array of words.
+ * @param IN is the path of the sequence file.
+ * @return the number of elements stored on words if everything 
+ * 		was OK and a negative number in other cases.
+ */
+int takeWords(wentry **words, char *IN){
+ 	// Variables
+	FILE *f;
+	char c;
+
+	// Open sequence file
+	if((f = fopen(IN,"rt"))==NULL){
+		fprintf(stderr, "Error openening sequence file\n");
+		return -1;
+	}
+
+	// Fasta files starts with a comment. Avoid it.
+	c = fgetc(f);
+	while(feof(f) & c != '\n')
+		c = fgetc(f);
+
+	// Check
+	if(feof(f)){
+		fprintf(stderr, "Error reading sequence file. Not sequence found.\n");
+		return -1;
+	}
+
+	// Start to read sequence
+	// Prepare workspace
+	unsigned long index = 0;
+	unsigned long inEntry = 0; // Length of the well formed sequence stored on the buffer
+	unsigned long NW = 0; // Number of well done sequences
+		unsigned long totC = 0; // Total of characters read
+		unsigned long NoACGT = 0; // Total of NoACGT character read on sequence
+		unsigned long NoSeq = 0; // Number of no-sequence lines
+	wentry temp; // Buffer
+	temp.seq = 0;
+
+	// Start to read
+	c = fgetc(f);
+	while(!feof(f)){
+		if (!isupper(toupper(c))){ // Comment, empty or quality (+) line
+			if(c=='>'){ // Comment line
+				c = fgetc(f);
+				while (c != '\n')
+					c = fgetc(f);
+				temp.seq++; // New sequence
+				inEntry = 0; // Reset buffer length
+				index++; //
+			}
+			NoSeq++;
+			c=fgetc(f);
+			continue;
+		}
+		shift_word(&temp.w); // Shift bits sequence
+		// Add new nucleotid
+		switch (c) {
+			case 'A': // A = 00 
+				inEntry++;
+				break;
+			case 'C': // C = 01
+				temp.w.b[BYTES_IN_WORD-1]|=1;
+				inEntry++;
+				break;
+			case 'G': // G = 10
+				temp.w.b[BYTES_IN_WORD-1]|=2;
+				inEntry++;
+				break;
+			case 'T': // T = 11
+				temp.w.b[BYTES_IN_WORD-1]|=3;
+				inEntry++;
+				break;
+			default : // Bad formed sequence
+				inEntry=0; NoACGT++; break;
+		}
+		index++;
+		totC++;
+		if(inEntry >= (unsigned long)WORD_SIZE){ // Full well formed sequence 
+			temp.pos=index-WORD_SIZE;
+			NW++;
+			if(storeWord(words,&temp,NW) < 0) return -1;
+		}
+		c=fgetc(f);
+	}
+
+	fclose(f); // Close input stream
+
+	return NW;
+}
+
+
 /*
  */
 void shift_word(word* w){
@@ -40,5 +134,101 @@ int storeWord(wentry** wArr,wentry *word,int length){
 	// Copy the new word
 	memcpy(wArr[length-1],word,sizeof(wentry));
 
+	return 0;
+}
+
+
+/* 	
+ */
+int quickSort(wentry** words, int left, int right){
+	int j;
+
+	if(left < right){
+		// divide and conquer
+		if((j = partition(words, left, right))<0) return -1;
+		quickSort(words, left, j-1);
+		quickSort(words, j+1, right);
+	}
+	return 0;
+}
+
+
+/*
+ */
+int partition(wentry** words, int left, int right){
+	int  i, j;
+	wentry* pivot;
+	wentry* temp;
+
+//	if((pivot = (wentry*) malloc(sizeof(wentry*)))==NULL){
+//		fprintf(stderr, "Error allocating memory for pivot.\n");
+//		return -1;
+//	}
+//	if((temp = (wentry*) malloc(sizeof(wentry*)))==NULL){
+//		fprintf(stderr, "Error allocating memory for auxiliar variable.\n");
+//		return -1;
+//	}
+
+	pivot = words[left];
+	i = left;
+	j = right+1;
+		
+	while(i >= j){
+		do ++i; while(wordComparator(words[i],pivot)<=0 && i <= right);
+		do --j; while(wordComparator(words[j],pivot) > 0);
+		if(i < j){
+			temp = words[i];
+			words[i] = words[j];
+			words[j] = temp;
+		}
+	}
+
+	temp = words[left];
+	words[left] = words[j];
+	words[j] = temp;
+	
+	return j;
+}
+
+
+/* This function is used to compare two wentry instances. The criterion
+ * used is:
+ * 		1 - Compare sequences (alphabetically).
+ * 		2 - Compare sequence index.
+ * 		3 - Compare position on sequence.
+ * @param w1 word to be compared.
+ * @param w2 word to be compared.
+ * @return a positive number if w1 is greater than w2, a negative number
+ * 		if w2 is greater than w1 and zero if both are equal.
+ */
+int wordComparator(wentry* w1,wentry* w2){
+	if(w1->w.b[0] > w2->w.b[0]) return 1;
+	else if(w1->w.b[0] < w2->w.b[0]) return -1;
+
+	if(w1->w.b[1] > w2->w.b[1]) return 1;
+	else if(w1->w.b[1] < w2->w.b[1]) return -1;
+
+	if(w1->w.b[2] > w2->w.b[2]) return 1;
+	else if(w1->w.b[2] < w2->w.b[2]) return -1;
+
+	if(w1->w.b[3] > w2->w.b[3]) return 1;
+	else if(w1->w.b[3] < w2->w.b[3]) return -1;
+
+	if(w1->w.b[4] > w2->w.b[4]) return 1;
+	else if(w1->w.b[4] < w2->w.b[4]) return -1;
+
+	if(w1->w.b[5] > w2->w.b[5]) return 1;
+	else if(w1->w.b[5] < w2->w.b[5]) return -1;
+
+	if(w1->w.b[6] > w2->w.b[6]) return 1;
+	else if(w1->w.b[6] < w2->w.b[6]) return -1;
+
+	if(w1->w.b[7] > w2->w.b[7]) return 1;
+	else if(w1->w.b[7] < w2->w.b[7]) return -1;
+
+	if(w1->seq > w2->seq) return 1;
+	else if(w1->seq < w2->seq) return -1;
+
+	if(w1->pos > w2->pos) return 1;
 	return 0;
 }
